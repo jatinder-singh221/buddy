e4f7a92777f9f77980d239349d7ec1a8da0d2ee3
jatinder-singh221 <jatindersing19@gmail.com>
2025-03-16 00:57:57 +0530

    feat taxcalculator

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,12 +5,6 @@ document.addEventListener("DOMContentLoaded", async () => {
         const header = document.createElement("nav")
         header.innerHTML = response
         document.body.prepend(header)
-
-        const location = window.location.pathname.replace('/', '')
-        const link = document.getElementById(`${location}`)
-        link.classList.remove('text-slate-700')
-        link.classList.add('font-semibold', 'text-[#6927DA]')
-
         const button = document.querySelector('#menu-button')
 
         button.addEventListener('click', () => {
@@ -18,7 +12,95 @@ document.addEventListener("DOMContentLoaded", async () => {
             menu.classList.toggle('hidden')
         })
 
+        const location = window.location.pathname.replace('/', '')
+        let link = ''
+        if (location == '') {
+            link = document.getElementById('index.html')
+        } else
+            link = document.getElementById(`${location}`)
+        link.classList.remove('text-slate-700')
+        link.classList.add('font-semibold', 'text-[#6927DA]')
+
+
+        document.querySelector("form").addEventListener("submit", handleCalculateTax)
+
+
+        if (location == 'tax-calculator.html') 
+            getTaxTableData()
+
+
     } catch (error) {
         console.error(error)
     }
 })
+
+const getSetLocalStorage = (name, data) => {
+    let existingData = JSON.parse(localStorage.getItem(name)) || []
+    existingData.push(data)
+    localStorage.setItem(name, JSON.stringify(existingData))
+}
+
+const getTaxTableData = () => {
+    const taxData = JSON.parse(localStorage.getItem("taxHistory"))
+    const historyBody = document.getElementById("historyBody")
+    const totalHistoryElement = document.getElementById('total-history-amount')
+    let totalSum = 0
+
+    historyBody.innerHTML = ""
+    
+    if (taxData){
+        taxData.forEach(item => {
+            const row = document.createElement("tr")
+            totalSum += item.total
+    
+            row.innerHTML = `
+            <td class="border border-gray-300 px-4 py-2">${item.name || 'Null'}</td>
+                <td class="border border-gray-300 px-4 py-2">${item.taxableValue}</td>
+                <td class="border border-gray-300 px-4 py-2">${item.percentage} %</td>
+                <td class="border border-gray-300 px-4 py-2">${item.csgstAmount}</td>
+                <td class="border border-gray-300 px-4 py-2">${item.total}</td>
+            `
+            historyBody.appendChild(row)
+        })
+    }
+    totalHistoryElement.innerHTML = totalSum
+}
+
+
+const handleCalculateTax = (event) => {
+    event.preventDefault()
+    const name = document.getElementById('name').value
+    const quantity = document.getElementById('quantity').value
+    const amount = document.getElementById('amount').value
+    const IGST = document.getElementById('IGST').value
+
+    const taxableValueElement = document.getElementById('taxable-value')
+    const cgstAmountElement = document.getElementById('cgst-amount')
+    const cgstPercentageElement = document.getElementById('cgst-percentage')
+    const totalElement = document.getElementById('total-amount')
+
+    const totalAmount = Math.round(quantity * amount)
+    const taxableAmount = Math.round(amount / (100 + parseInt(IGST)) * 100)
+
+    totalElement.innerHTML = totalAmount
+    taxableValueElement.innerHTML = taxableAmount
+    cgstPercentageElement.innerHTML = parseInt(IGST) / 2
+    cgstAmountElement.innerHTML = (totalAmount - taxableAmount) / 2
+
+    const data = {
+        'name': name || '',
+        'taxableValue': taxableAmount,
+        'percentage': parseInt(IGST) / 2,
+        'csgstAmount': (totalAmount - taxableAmount) / 2,
+        'total': totalAmount
+    }
+
+    getSetLocalStorage('taxHistory', data)
+    getTaxTableData()
+}
+
+
+const clearStorage = (name) => {
+    window.localStorage.removeItem(name)
+    getTaxTableData()
+}
